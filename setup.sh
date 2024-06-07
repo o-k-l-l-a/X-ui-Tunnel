b6dfbb4ce4cda1ed661f95bd60d4261b02a629f5
O»K»I»L»A <89906412+o-k-l-l-a@users.noreply.github.com>
2024-06-07 04:47:59 +0330

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Prompt user for location choice
+read -p "Please choose your location (iran/uk): " location
+
+# Define the URL of the setup script based on user's choice
+case "$location" in
+    iran)
+        url="https://raw.githubusercontent.com/o-k-l-l-a/X-ui-Tunnel/main/src/iran.sh"
+        ;;
+    uk)
+        url="https://raw.githubusercontent.com/o-k-l-l-a/X-ui-Tunnel/main/src/uk.sh"
+        ;;
+    *)
+        echo "Invalid choice. Please enter 'iran' or 'uk'."
+        exit 1
+        ;;
+esac
+
+# Download and execute the setup script
+curl -o setup.sh "$url" && bash setup.sh
+
+# Clean up by removing the downloaded script
+rm setup.sh
